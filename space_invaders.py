989fa549cadf2ee89d57b1722a814f169b603e55
Chris <cf_anderson@netzero.com>
2019-07-24 09:11:36 -0500

    Improved movement

--- space_invaders.py
+++ space_invaders.py
@@ -10,28 +10,30 @@ title_font = pygame.font.SysFont('Arial', 26, True)
 pygame.display.set_caption('SPACE INVADERS!')
 
 
+x_coordinate = 50
+y_coordinate = 100
 
 def handle_events():
+    global x_coordinate, y_coordinate
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            snake.is_alive = False
+            is_playing = False
         elif event.type == pygame.KEYDOWN:
-            key_left = event.key in (pygame.K_LEFT, pygame.K_a)
-            key_right = event.key in (pygame.K_RIGHT, pygame.K_d)
-            key_up = event.key in (pygame.K_UP, pygame.K_w)
-            key_down = event.key in (pygame.K_DOWN, pygame.K_s)
-            if key_left:
-                snake.set_direction_left()
-            elif key_right:
-                snake.set_direction_right()
+            if event.key == pygame.K_LEFT:
+                x_coordinate = x_coordinate - 10
+            elif event.key == pygame.K_RIGHT:
+                y_coordinate = y_coordinate + 10
 
-    # Main game loop
+      # Main game loop
     is_playing = True
     while is_playing:
+
+        handle_events()
+
         game_display.blit(game_display, (0, 0))
         
         game_display.fill((0, 0, 0))
-        pygame.draw.rect(game_display, (0 , 255, 0), pygame.Rect(50, 50, 20, 20))
+        pygame.draw.rect(game_display, (0 , 255, 0), pygame.Rect(x_coordinate, y_coordinate, 20, 20))
         # score_text = score_font.render(str(snake.score), False, (255, 255, 255))
         # game_display.blit(score_text, (0,0))
 
