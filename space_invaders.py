a2f97dcce357003c8e475c95445dff9f1f6f7bd5
Chris <cf_anderson@netzero.com>
2019-07-24 10:49:45 -0500

    Added variables to make code more readable

--- space_invaders.py
+++ space_invaders.py
@@ -2,19 +2,36 @@
 # Choose an interpreter that works
 import pygame
 
+# Game settings
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 600
+GAME_SIDE_MARGIN = 20
+GAME_TOP_MARGIN = 20
+GAME_BOTTOM_MARGIN = 20
+GAME_BORDER_WIDTH = 3
+
+GAME_TOP_WALL = GAME_TOP_MARGIN + GAME_BORDER_WIDTH
+GAME_RIGHT_WALL = WINDOW_WIDTH - GAME_SIDE_MARGIN - GAME_BORDER_WIDTH
+GAME_BOTTOM_WALL = WINDOW_HEIGHT - GAME_BOTTOM_MARGIN - GAME_BORDER_WIDTH
+GAME_LEFT_WALL = GAME_SIDE_MARGIN + GAME_BORDER_WIDTH
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 # Media files
 player_image = pygame.image.load('si-player.gif')
 
 pygame.init()
 clock = pygame.time.Clock()
-game_display = pygame.display.set_mode((400, 600))
+game_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 score_font = pygame.font.SysFont('Arial', 22, True)
 title_font = pygame.font.SysFont('Arial', 26, True)
 pygame.display.set_caption('SPACE INVADERS!')
 
 
 x_coordinate = 200
-y_coordinate = 575
+y_coordinate = GAME_BOTTOM_WALL - player_image.get_height()
 should_move_right = False
 should_move_left = False
 
@@ -49,7 +66,18 @@ while is_playing:
 
     game_display.blit(game_display, (0, 0))
 
-    game_display.fill((0, 0, 0))
+    game_display.fill((BLACK))
+
+    pygame.draw.rect(game_display, WHITE, \
+                    (GAME_SIDE_MARGIN, GAME_TOP_MARGIN, \
+                    WINDOW_WIDTH - GAME_SIDE_MARGIN * 2, \
+                    WINDOW_HEIGHT - GAME_BOTTOM_MARGIN * 2))
+
+    pygame.draw.rect(game_display, BLACK, \
+                    (GAME_LEFT_WALL, GAME_TOP_WALL, \
+                    WINDOW_WIDTH - GAME_LEFT_WALL - GAME_SIDE_MARGIN - GAME_BORDER_WIDTH, \
+                    WINDOW_HEIGHT - GAME_TOP_WALL - GAME_BOTTOM_MARGIN - GAME_BORDER_WIDTH))
+
     game_display.blit(player_image, (x_coordinate, y_coordinate))
     # score_text = score_font.render(str(snake.score), False, (255, 255, 255))
     # game_display.blit(score_text, (0,0))
